1b762f9b8ac53147e8d4950779ede97f62883de1
Aslan.K.Kardanov <Aslan.K.kardanov@rt.ru>
2024-09-29 23:43:38 +0300

    added commit_02

--- src/test/java/qa/guru/owner/WebDriverTest.java
+++ src/test/java/qa/guru/owner/WebDriverTest.java
@@ -1,5 +1,6 @@
 package qa.guru.owner;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
@@ -9,11 +10,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebDriverTest {
     @Test
-    public  void testGuthub(){
+    public  void testGithub(){
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://github.com/");
         String title  = driver.getTitle();
         assertEquals("GitHub: Let’s build from here · GitHub", title);
+        driver.quit();
 
     }
 }
