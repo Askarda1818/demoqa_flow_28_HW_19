0041ac9ed5f5c672f23e7662d89d1398ba951548
Aslan.K.Kardanov <Aslan.K.kardanov@rt.ru>
2024-10-02 23:37:21 +0300

    added commit_04

--- src/test/java/qa/guru/owner/WebDriverTest.java
+++ src/test/java/qa/guru/owner/WebDriverTest.java
@@ -1,30 +1,42 @@
 package qa.guru.owner;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import qa.guru.owner.config.WebDriverProvider;
 
 import static java.sql.DriverManager.getDriver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebDriverTest {
-    private WebDriver driver;
+   // private WebDriver driver;
+   private  WebDriver driver;
 
     @BeforeEach
     public void startDriver(){
-       // driver = getDriver();
+        // Подготовка драйвера
+        driver = new WebDriverProvider().get();
     }
     @Test
     public  void testGithub(){
+        // тело выполнения тесте
+        String title  = driver.getTitle();
+        assertEquals("GitHub: Let’s build from here · GitHub", title);
+
+    }
+    @AfterEach
+    public void stopDriver(){
+        driver.quit();
+    }
+
+    /*private WebDriver getDriver(){
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://github.com/");
-        String title  = driver.getTitle();
-        assertEquals("GitHub: Let’s build from here · GitHub", title);
-        driver.quit();
-
-    }
+        return driver;
+    }*/
 }
--- src/test/java/qa/guru/owner/config/WebDriverProvider.java
+++ src/test/java/qa/guru/owner/config/WebDriverProvider.java
@@ -3,15 +3,36 @@ package qa.guru.owner.config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.function.Supplier;
 
 public class WebDriverProvider implements Supplier<WebDriver> {
+
+    public WebDriverProvider(){
+       this.config = new WebDriverConfig();
+    }
+
+    private final WebDriverConfig config;
     @Override
     public WebDriver get(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://github.com/");
+      WebDriver driver = createWebDriver();
+        driver.get(config.getBaseUrl());
         return driver;
+    }
+    public WebDriver createWebDriver(){
+        switch (config.getBrowser()){
+            case CHROME:{
+                WebDriverManager.chromedriver().setup();
+                return  new ChromeDriver();
+            }
+            case FIREFOX:{
+                WebDriverManager.firefoxdriver().setup();
+                return  new FirefoxDriver();
+            }
+            default:{
+                throw new RuntimeException("No such driver");
+            }
+        }
     }
 }
